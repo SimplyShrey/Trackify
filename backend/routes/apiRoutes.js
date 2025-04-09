4a3525fe9cb6e9c75e8a7cd9a60fc3399ae9bb0c
Shreyas <144884676+SimplyShrey@users.noreply.github.com>
2025-04-09 12:01:39 +0530

    Update apiRoutes.js
    Code updated in spotiftyRoutes. apiRoutes served the same functionality hence is not needed anymore.

--- backend/routes/apiRoutes.js
+++ backend/routes/apiRoutes.js
@@ -1,4 +1,4 @@
-const express = require('express');
+/*const express = require('express');
 const axios = require('axios');
 const pool = require('../config/db');
 const router = express.Router();
@@ -22,4 +22,4 @@ router.get('/me', async (req, res) => {
 });
 
 
-module.exports = router;
+module.exports = router;*/
